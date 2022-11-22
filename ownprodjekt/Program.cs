f360f8f28c45c4c07be4ae23a1ff6cf53725cdc3
EdvinGitHub <Edvin.arvesjo@elev.ga.ntig.se>
2022-11-22 10:13:43 +0100

    uyjmuyrfmjy

---
+++ ownprodjekt/Program.cs
@@ -0,0 +1,20 @@
+﻿// this game is a child game
+// dont for get to use coments 
+using Raylib_cs;
+
+Raylib.InitWindow(1000,800,"Fuck you");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.GRAY);
+
+
+    Raylib.DrawRectangle(100, 100, 100, 100, Color.RED);
+    
+    Raylib.EndDrawing();
+}
+
+Console.ReadLine();
